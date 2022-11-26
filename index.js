7b2ff6b9a53312e797a81987de7929ce22b3b7a7
KellyMark <kmark602@gmail.com>
2022-11-26 17:31:42 -0500

    commit tree d6acbb8f7c933456542ede9e7067ebdf7367b678

---
+++ index.js
@@ -0,0 +1,35 @@
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+function increment(n) {
+    n++;
+    return n;
+}
+
+function decrement(n) {
+    n--;
+    return n;
+}
+
+function makeInt(n) {
+    const m = parseInt(n, 10);
+    return m;
+}
+
+function preserveDecimal(n) {
+    const m = parseFloat(n);
+    return m;
+}
